db818880b789d08a402024bd3466e7ce3ff51371
mentzelia <mentzelia@hotmail.fr>
2018-11-27 16:59:37 -1000

    timer operationnel procedural

--- timer.js
+++ timer.js
@@ -1,7 +1,8 @@
 var ElementHtmlSeconde = document.getElementById("timerSeconde");
 var ElementHtmlMinute = document.getElementById("timerMinute");
 
-function decompterSeconde() {    
+
+ function decompterSeconde() {    
     var compteurSeconde = Number(ElementHtmlSeconde.textContent);
     var compteurMinute = Number(ElementHtmlMinute.textContent);
     
@@ -12,14 +13,17 @@ function decompterSeconde() {
             ElementHtmlSeconde.textContent= compteurSeconde+59;
             ElementHtmlMinute.textContent= compteurMinute-1;
         } 
-    }
+    };
     
     if(compteurMinute===0 && compteurSeconde===0) {
                         clearInterval(setCompteur);
                         var reservation= document.getElementById("reservation");
                         reservation.textContent= "Votre réservation a expirée."
-};
+    };
+ };
 
-var setCompteur= setInterval(decompterSeconde,1000);
+
+ var setCompteur= setInterval(decompterSeconde,1000);
+
+ 
   
-    
